a2dc3a4293af86401495695727bf6b589691ffbf
skepttalk <kuldeepsingh861915@gmail.com>
2025-09-12 11:48:40 +0530

    chore: .env

---
+++ src/Components/parctice.jsx
@@ -0,0 +1,36 @@
+// import React, { use, useEffect, useState } from 'react'
+
+// const parctice = () => {
+//     // const [WeatherData,setWeatherData] = useState({})
+//     const[Header,setHeaderData]  = useState({})
+//     useEffect(()= {
+
+//     //     fetch("http://api.weatherstack.com/current?access_key=a4c7c96e6d7466e2e8cc19604947a98c&query=india");
+
+//     //     .then((res) => res.json())
+//     //     .then((data)=>{
+//     //         setWeatherData(data);
+//     //         localStorage.setItem("weatherData,",json.stringify(data))
+//     //     })
+//     //     .catch((err)=> console.log(err))
+
+
+//     const data= JSON.parse(localStorage.getItem("weatherData"))
+//     console.log(data,"weatherData")
+
+// setHeaderData({city:data.location.name})
+
+//     },[]);
+
+
+
+
+//   return (
+
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default parctice
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -3,42 +3,38 @@ import Header from '../Components/Header'
 import MainBody from '../Components/MainBody'
 
 const MainPage = () => {
+  
+const AccessKey = import.meta.env.WEATHER_API_KEY
+console.log(AccessKey,"AccessKey")
+
+
   const[headerData,setHeaderData] = useState({});
   const[MainBodyData,setMainBodyData] = useState({})
-// const [WeatherData,setWeatherData] = useState({})
+const [WeatherData,setWeatherData] = useState({})
 
 useEffect(()=>{
-//   fetch("http://api.weatherstack.com/current?access_key=96386b40c2d300172aa59ccec8a3fa49&query=india")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setWeatherData(data);
-//       localStorage.setItem('weatherData', JSON.stringify(data)); 
-//     })
-//     .catch((err)=>console.log(err));
-  const data = JSON.parse(localStorage.getItem("weatherData"));
-  console.log(data, "weatherData");
-  
-  setHeaderData({city:data.location.name, temperature:data.current.temperature, weather_descriptions:data.current.weather_descriptions[0],Time:data.location.localtime});
-  // setMainBodyData({feels:data.current.feelslike, wind:data.current.wind_speed, prec:data.current.precip, uv:data.current.uv_index })
-
-},[]);
-
-useEffect(()=>{
-
+  fetch(`http://api.weatherstack.com/current?access_key=${AccessKey}&query=india, Rajasthan, Udaipur`)
+    .then((res) => res.json())
+    .then((data) => {
+      setWeatherData(data);
+      localStorage.setItem('weatherData', JSON.stringify(data)); 
+    })
+    .catch((err)=>console.log(err));
   const data = JSON.parse(localStorage.getItem("weatherData"));
   console.log(data, "weatherData");
 
   setMainBodyData({feels:data.current.feelslike, wind:data.current.wind_speed, prec:data.current.precip, uv:data.current.uv_index })
+  
+  setHeaderData({city:data.location.name, temperature:data.current.temperature, weather_descriptions:data.current.weather_descriptions[0],Time:data.location.localtime});
+
 
 },[]);
-
-
   return (
     <>
         <Header data={headerData}/>
         <MainBody key="main-body" data={MainBodyData} />
     </>
   )
-}
+}                                                                           
 
 export default MainPage
